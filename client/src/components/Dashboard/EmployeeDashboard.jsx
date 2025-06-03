2291216b2475ba8ad6bc558895faf33612a84a5b
Gaurav Sharma <gs236934@gmail.com>
2025-06-03 20:27:35 +0530

    employee dashboard in progress

--- client/src/components/Dashboard/EmployeeDashboard.jsx
+++ client/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../../other/Header'
+import TaskState from '../../other/TaskState'
+import TaskList from '../TaskList/TaskList'
 
 function EmployeeDashboard() {
   return (
-    <div>
-      employe Dashboard
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+      <Header />
+      <TaskState />
+      <TaskList />
     </div>
   )
 }
